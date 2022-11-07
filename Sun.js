f37085cf08e203d715ae59c52b67c70bd5d9e8d8
Israel Aristide <44146685+IsraelAristide@users.noreply.github.com>
2022-11-06 23:53:13 -0500

    remove test code

--- Sun.js
+++ Sun.js
@@ -4,7 +4,7 @@ class Sun {
         this.pos = createVector(width /2, height /2);
         this.rays = [];
 
-        for (let a = 0; a < 360; a+=10) {
+        for (let a = 0; a < 360; a+=.5) {
             this.rays.push(new Ray(this.pos, radians(a)));
         }
     }
@@ -27,9 +27,9 @@ class Sun {
 
     look(walls) {
 
-        //for (let ray of this.rays) {
+        for (let ray of this.rays) {
 
-            let ray = this.rays[25];
+            //let ray = this.rays[25];
             let record = Infinity;
             let closest = null;
             let closestwall = null;
@@ -50,43 +50,12 @@ class Sun {
 
             if (closest) {
                 
-                
                 stroke(225, 100)
-
                 line(this.pos.x, this.pos.y, closest.x, closest.y)
-                
-                let arrivingvector = createVector();
-                arrivingvector.x = closest.x - this.pos.x;
-                arrivingvector.y = closest.y - this.pos.y;
-
-                let x = this.pos.x
-                let y = closestwall.NormEquasion(x, closest);
-
-                let normalvector = createVector();
-                normalvector.x = x - closest.x;
-                normalvector.y = y - closest.y;
-                
-
-                normalvector.normalize();
-                arrivingvector.normalize();
-
-
-                let theta = p5.Vector.dot(arrivingvector, normalvector) / arrivingvector.mag() * normalvector.mag()
-                console.log("Angle between intersect and normal: " + degrees(Math.acos(theta)))
-                theta = radians(90) + Math.acos(theta)
-
-                console.log("Reflect Angle: " + degrees(theta))
-    
-
-                let nRay = new Ray(closest, theta);
-                nRay.show()
-                stroke(225, 100, 10, 100)
-                line(closest.x, closest.y, x, y)
-                
 
             }
 
-        //}
+        }
 
     }
 
